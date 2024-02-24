e498976f117e867e65d03d08b0761fbc221751d2
nlekkerman <nlekkerman@gmail.com>
2024-02-24 18:14:04 +0000

    Added riddle level.

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ This module contains functions related to the Lock Cracker game.
 """
 import shutil
 import random
+import re
 import time
 from colorama import init, Fore, Back, Style
 import gspread
@@ -25,13 +26,81 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # Open the specific spreadsheet
 SPREADSHEET = 'save_waldo'
 SHEET = GSPREAD_CLIENT.open(SPREADSHEET)
-# Specify the worksheet to use
-WORKSHEET_NAME = 'players'
-worksheet = SHEET.worksheet(WORKSHEET_NAME)
+
+# players sheet
+PLAYERS_WORKSHEET = 'players'
+worksheet_players = SHEET.worksheet(PLAYERS_WORKSHEET)
+
+# players sheet
+RIDDLES_WORKSHEET = 'riddles'
+worksheet_riddles = SHEET.worksheet(RIDDLES_WORKSHEET)
+
+
+def upload_riddles_to_worksheet(worksheet):
+    """
+    Uploads a predefined list of riddles to a specified worksheet.
+
+    Args:
+        worksheet (Worksheet): The worksheet object where the riddles will be uploaded.
+
+    Returns:
+        None
+    """
+    # Define the list of riddles with their answers
+    riddles = [
+        ["What can you break without touching it?", "promise"],
+        ["What has a heart that doesn't beat?", "n artichoke"],
+        ["What has one eye but can't see?", "needle"],
+        ["What has a face and two hands, but no arms or legs?", "clock"],
+        ["What comes once in a minute, twice in a moment, but never in a thousand years?", "The letter 'M'"],
+        ["What has many keys but can't open a single lock?", "typewriter"],
+        ["I’m full of holes, yet I can hold water. What am I?", "net"],
+        ["What has a bed but never sleeps, can run but never walks?", "river"],
+        ["What can travel around the world while staying in a corner?", "stamp"],
+        ["What has a neck but no head?", "bottle"],
+        ["What has keys but can't open locks?", "piano"],
+        ["What has hands but can't clap?", "clock"],
+        ["What belongs to you but others use it more than you do?", "Your name"],
+        ["What can be heard and caught but never seen?", "cold"],
+        ["What is so fragile that saying its name breaks it?", "Silence"],
+        ["What is full of holes but still holds water?", "sponge"]
+    ]
+
+
+    # Clear existing data in the worksheet
+    worksheet.clear()
+
+    # Define headers for the riddles
+    headers = ["Riddle", "Answer"]
+
+    # Append headers to the worksheet
+    worksheet.append_row(headers)
+
+    # Append each riddle and its answer to the worksheet
+    for riddle, answer in riddles:
+        worksheet.append_row([riddle, answer])
 
 
 # Inputs related function
 def input_for_saving_info(prompt):
+    """
+    Prompt the user for input with explanatory text and a green background.
+
+    Parameters:
+    - prompt (str): The prompt message to display to the user.
+
+    Returns:
+    - str: The user's input.
+    """
+
+    # Print the prompt with a green background
+    print(Fore.YELLOW + prompt + Style.RESET_ALL, end='')
+    # Get user input
+    user_input = input()
+    return user_input
+
+
+def input_for_password_level(prompt):
     """
     Prompt the user for input with explanatory text and a green background.
 
@@ -90,7 +159,7 @@ def greet_player_and_explain_game():
           + Style.RESET_ALL)
     time.sleep(1)
     print(Fore.MAGENTA + "She ruled the Bureaucracy Kingdom from her castle"
-          " on the top of Paper Mountain." + Style.RESET_ALL)3121
+          " on the top of Paper Mountain." + Style.RESET_ALL)
     time.sleep(1)
     print(Fore.MAGENTA +
           "In this quest, we will go through challenges to save Waldo,"
@@ -108,14 +177,14 @@ def collect_player_info():
     """
     Collect player's name and location, and save it to Google Sheets.
     """
-    print_input_instructions("Enter your name please")
+    print_input_instructions("Enter your name please", Fore.WHITE)
     name = input_for_saving_info("Enter your name: ")
 
-    print_input_instructions("Enter your location please")
+    print_input_instructions("Enter your location please", Fore.WHITE)
     location = input_for_saving_info("Enter your location: ")
 
     # Append player information to the worksheet
-    worksheet.append_row([name, location])
+    worksheet_players.append_row([name, location])
     print()
     print(f"Welcome to adventure {Fore.YELLOW}{name}{Fore.RESET} of "
           f"{Fore.YELLOW}{location}{Fore.RESET} let's save the Waldo.")
@@ -172,15 +241,16 @@ def print_centered_text(text, color):
     )
     print()
 
-
+"""
 # PLAY GAME CALLS
+upload_riddles_to_worksheet(worksheet_riddles)
 greet_player_and_explain_game()
 collect_player_info()
 time.sleep(0.5)
 print_centered_text("Challenge one, CRACK THE LOCK!!", Fore.RED)
 time.sleep(0.5)
 print_password_challenge_instructions()
-
+"""
 
 # PASSWORD LEVEL FUNCTIONS
 def generate_password():
@@ -198,13 +268,12 @@ def provide_hint(password):
     return hint_numbers
 
 
-def reveal_password(password, guessed_numbers):
+def reveal_password(password, guessed_numbers, revealed_positions):
     """
     Reveals the correct numbers guessed by the player.
     """
     revealed_numbers = [
-        '?' if i not in guessed_numbers else str(password[i])
-        for i in range(4)
+        str(password[i]) if i in revealed_positions else '?' for i in range(4)
     ]
     return ' '.join(revealed_numbers)
 
@@ -219,14 +288,42 @@ def play_password_level():
     # Provide hint to the player
     hint_numbers = provide_hint(password)
 
-    print("Welcome to the Password Guessing Game!")
-    print("Try to guess the 4-digit password.")
-    print("Hint: You have these two numbers in the password:", hint_numbers)
+    print()
+    print()
+    print(Fore.RESET + Back.YELLOW + Fore.WHITE +
+          "   ADVENTURER   " + Style.RESET_ALL)
+    print()
+    time.sleep(1)
+    print(Back.WHITE + Fore.CYAN +
+          " Now, I need you to stay focused! " + Style.RESET_ALL)
+    print()
+    time.sleep(1)
+    print(Back.WHITE + Fore.CYAN +
+          " You have to crack this lock to enter this castle! "
+          + Style.RESET_ALL)
+    print()
+    time.sleep(1)
+    print(Back.WHITE + Fore.CYAN +
+          " Let's go and crack this 4-digit password! "
+          + Style.RESET_ALL)
+    print()
+    time.sleep(1)
+    print(Back.YELLOW + Fore.WHITE +
+          " Hint: You have these two numbers in the password: "
+          + Style.RESET_ALL +
+          Fore.GREEN +
+          f'({", ".join(map(str, hint_numbers))})' +
+          Fore.RESET)
+
+    # Initialize revealed positions to an empty list
+    revealed_positions = []
 
     # Game loop
     while True:
         # Ask the player to guess the password
-        guess = input("Enter your guess (4-digit number without spaces): ")
+        print_input_instructions(
+            "Enter your guess (4-digit number without spaces): ", Fore.YELLOW)
+        guess = input_for_password_level("Enter 4-digits without spaces): ")
 
         # Check if the guess is correct
         if len(guess) != 4 or not guess.isdigit():
@@ -235,22 +332,72 @@ def play_password_level():
 
         guess = [int(digit) for digit in guess]
         if guess == password:
-            print("Congratulations! You've guessed the correct password:", ''.join(map(str, password)))
+            print(Fore.WHITE + Back.GREEN +
+                  "Congratulations! You are in: " + Style.RESET_ALL + ' ',
+                  Fore.YELLOW + Back.RED + "Password was " + ''.join(map(str, password))
+                  + Style.RESET_ALL)
             break
-        else:
-            revealed_password = reveal_password(password, hint_numbers)
-            print("Incorrect guess. Here's the revealed part of the password:", revealed_password)
-            print("Please try again.")
 
+        # Update revealed positions if any correct number is guessed
+        for i in range(4):
+            if guess[i] == password[i]:
+                revealed_positions.append(i)
 
+        revealed_password = reveal_password(
+            password, hint_numbers, revealed_positions)
+        print(Fore.RED + "Incorrect guess."
+              " Here's the revealed part of the password:"
+              + Fore.RESET + Fore.YELLOW, revealed_password
+              + Style.RESET_ALL)
+        print(Fore.RED + "Please try again." + Style.RESET_ALL)
+
+#RIDDLE FUNCTIONS
+def play_riddle_level():
+    """
+    Function to play the riddle level.
+    """
+    global SHEET
+
+    print("\nYou have entered the castle and encountered the ghost of Enigma.")
+    print("To proceed further, you must answer a riddle correctly.")
+    print("You have 5 attempts to answer the riddle.")
+
+    # Fetch riddles from the worksheet
+    riddles_data = worksheet_riddles.get_all_values()[1:]  # Exclude header row
+
+    # Set a random seed for reproducibility
+    random.seed()
+    random_riddle = random.choice(riddles_data)
+    riddle, answer = random_riddle
+
+    attempts = 5
+    while attempts > 0:
+        print("\nRiddle:", riddle)
+        try:
+            user_answer = input("Your answer: ")
+            # Validate user input
+            if not re.match("^[a-zA-Z0-9\s]*$", user_answer):
+                raise ValueError("Invalid input. Answer can only contain letters, spaces, and numbers.")
+            # Check if user's answer is correct
+            if user_answer.lower() == answer.lower():
+                print("Congratulations! You have answered correctly.")
+                break
+            else:
+                attempts -= 1
+                if attempts > 0:
+                    print("Incorrect answer. You have", attempts, "attempts remaining.")
+                else:
+                    print("Incorrect answer. You have run out of attempts. The ghost of Enigma has defeated you.")
+        except ValueError as e:
+            print(e)
 # MAIN FUNCTION
 def main():
     """
     Main function to orchestrate the game flow.
     """
 
-    play_password_level()
-
+   # play_password_level()
+    play_riddle_level()
 
 if __name__ == "__main__":
     main()
